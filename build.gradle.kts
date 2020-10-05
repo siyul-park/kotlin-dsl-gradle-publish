9af223a7eea99453c48db6d4a2110d7507355c86
siyual-park <siyual.bak@gmail.com>
2020-10-05 09:16:44 +0900

    add bintray-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,38 @@
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.novoda", "bintray-release", "0.9.2")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.4.10"
 }
 
-group = "org.example"
+apply(plugin = "com.novoda.bintray-release")
+
+group = "com.ara.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+configure<com.novoda.gradle.release.PublishExtension> {
+    bintrayUser = System.getenv("BINTRAY_USER")
+    bintrayKey = System.getenv("BINTRAY_API_KEY")
+
+    userOrg = "ara"
+    groupId = group as String?
+    repoName = "kotlin-dsl-gradle-publish"
+    publishVersion = version as String?
+    desc = "Example for publish with kotlib dsl"
+    website = "https://github.com/siyual-park/kotlin-dsl-gradle-publish"
+    dryRun = false
+    override = true
 }
 
 dependencies {
